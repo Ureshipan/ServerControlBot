6aec832906ba57efc06c805b2a00b0200b1592fc
m0tr1x <gamervolga@gmail.com>
2023-02-27 17:44:39 +0300

    Закинул функцию для бекапа

--- Current_Run/main.py
+++ Current_Run/main.py
@@ -1,4 +1,5 @@
 import logging
+import subprocess #Чтобы взаимодействовать с bash консолью линукса
 from aiogram import Bot, Dispatcher, executor, types
 
 API_TOKEN = '6070379081:AAGrYcJlgTrVuoVAeRJjvHZjPwbxqSjfvJI' #тестить здесь @PrjctServerControllerBot
@@ -25,6 +26,11 @@ async def send_welcome(message: types.Message):
 async def echo(message: types.Message):
     await message.answer(message.text)
 
+@dp.message_handler(commands=['/backup_cur'])
+async def backup(message: types.Message):
+    subprocess.call("mv Current_Run/* Backup", shell=True)
+    subprocess.call("cp Backup/* Current_Run", shell=True)
+    #Буквально пишем баш команды через интерфейс этой библы
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
